6a8b1f0e90ebfc1c15dc8439cf9d405c13a152f3
Qusay Diaz <Qusay_Diaz.github@outlook.com>
2025-05-30 21:03:19 +0800

    主界面优化

--- cat2/Views/MainWindow.xaml.cs
+++ cat2/Views/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using ChmlFrp.SDK;
 using ChmlFrp.SDK.API;
 using Wpf.Ui.Appearance;
 using Wpf.Ui.Controls;
@@ -13,17 +14,14 @@ public partial class MainWindow
     public MainWindow()
     {
         InitializeComponent();
-        Loaded += MainWindow_OnLoaded;
     }
 
-    private async void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+    private async void OnLoaded(object sender, RoutedEventArgs e)
     {
         // 系统主题
         ApplicationThemeManager.Apply(ApplicationThemeManager.GetSystemTheme() == SystemTheme.Dark
             ? ApplicationTheme.Dark
             : ApplicationTheme.Light);
-
-        RootNavigation.Navigate(typeof(ProgressPage));
 
         GlobalSnackbar = new Snackbar(new SnackbarPresenter())
         {
@@ -35,18 +33,24 @@ public partial class MainWindow
         };
         MainGrid.Children.Add(GlobalSnackbar);
 
+        Paths.Init("CAT2");
+
         await Sign.Signin();
-        Topmost = false;
         if (Sign.IsSignin)
         {
             UserItem.Visibility = Visibility.Visible;
+            TunnelItem.Visibility = Visibility.Visible;
             RootNavigation.Navigate("用户页");
         }
         else
         {
             LoginItem.Visibility = Visibility.Visible;
+            LoadingRing.IsIndeterminate = false;
+            LoadingRing.Visibility = Visibility.Collapsed;
             RootNavigation.Navigate("登录");
         }
+
+        Topmost = false;
     }
 
     private void ThemesChanged(object sender, RoutedEventArgs e)
