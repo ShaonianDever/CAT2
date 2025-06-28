7361f4bfd4cc024e4b70585370a7507c5d703db7
Qusay Diaz <Qusay_Diaz.github@outlook.com>
2025-06-28 18:20:03 +0800

    化身成派萌你的最好导游<(￣︶￣)↗[GO!]

--- cat2/Model.cs
+++ cat2/Model.cs
@@ -13,7 +13,19 @@ public abstract class Model
 {
     public static readonly MainWindow MainClass = (MainWindow)Application.Current.MainWindow;
     public static readonly string Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
+
+    public static readonly string FileVersion = Assembly
+        .GetExecutingAssembly()
+        .GetCustomAttribute<AssemblyFileVersionAttribute>()?
+        .Version;
+
+    public static readonly string Copyright = Assembly
+        .GetExecutingAssembly()
+        .GetCustomAttribute<AssemblyCopyrightAttribute>()?
+        .Copyright;
+
     public static readonly string AssemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+
 
     public static async void ShowTip(string title, string content, ControlAppearance appearance, SymbolRegular icon)
     {
--- cat2/ViewModels/SettingPageViewModel.cs
+++ cat2/ViewModels/SettingPageViewModel.cs
@@ -3,5 +3,7 @@
 public partial class SettingPageViewModel : ObservableObject
 {
     [ObservableProperty] private string _assemblyName = Model.AssemblyName;
+    [ObservableProperty] private string _copyright = Model.Copyright;
+    [ObservableProperty] private string _fileVersion = $"文件版本：{Model.FileVersion}";
     [ObservableProperty] private string _version = Model.Version;
 }
