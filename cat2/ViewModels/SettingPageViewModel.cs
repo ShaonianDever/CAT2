d6814b602fd469f0db38e52cdec380d1e7ec2131
Qusay Diaz <Qusay_Diaz.github@outlook.com>
2025-07-06 11:56:36 +0800

    Add cache clearing functionality (wait for using) and enhance UI in settings

--- cat2/ViewModels/SettingPageViewModel.cs
+++ cat2/ViewModels/SettingPageViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace CAT2.ViewModels;
+﻿using System.IO;
+
+namespace CAT2.ViewModels;
 
 public partial class SettingPageViewModel : ObservableObject
 {
@@ -6,4 +8,24 @@ public partial class SettingPageViewModel : ObservableObject
     [ObservableProperty] private string _copyright = Model.Copyright;
     [ObservableProperty] private string _fileVersion = $"文件版本：{Model.FileVersion}";
     [ObservableProperty] private string _version = Model.Version;
+
+    [RelayCommand]
+    private void ClearCache()
+    {
+        foreach (var cachefile in Directory.GetFiles(DataPath, "*.log"))
+            try
+            {
+                File.Delete(cachefile);
+            }
+            catch
+            {
+                // ignored
+            }
+
+        ShowTip(
+            "缓存已清理",
+            "所有缓存文件已被删除。",
+            ControlAppearance.Success,
+            SymbolRegular.PresenceAvailable24);
+    }
 }
